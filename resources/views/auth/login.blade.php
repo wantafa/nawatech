d9862364fbd35efd78192508e410070572639671
wantafa <dakwan.taqiyyuddin@gmail.com>
2023-06-03 12:15:18 +0700

    update link Register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -185,7 +185,7 @@
 
               <p class="text-center">
                 <span>New on our platform?</span>
-                <a href="auth-register-basic.html">
+                <a href="/register">
                   <span>Create an account</span>
                 </a>
               </p>
